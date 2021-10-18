dbef266ad121b9ac8b8e9f1e3d18fde6b16e2e56
Freaky <freaky2112@hotmail.com>
2021-10-18 20:26:11 +0000

    add tmux and htop

--- aliases.sh
+++ aliases.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cat aliases.txt >> ~/.bashrc
-echo -e " ## My Aliases ## \n SSH AGENT { ssha } \n Distro Upgrade { upgrade } \n Install essentiel { git curl wget net-tools } \n What is  my ip { wip }\n"
+echo -e " ## My Aliases ## \n SSH AGENT { ssha } \n Distro Upgrade { upgrade } \n Install essentiel { git curl wget net-tools tmux htop} \n What is  my ip { wip }\n"
